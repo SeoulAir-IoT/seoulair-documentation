85bc4a0e9fe218d4e74f54634631d48d5fa9ca03
uros.aleksandrovic <uros.aleksandrovic@outlook.com>
2021-02-08 22:10:53 +0100

    Add Shell scripts.

---
+++ shell-scripts/seoulair-clean.sh
@@ -0,0 +1,59 @@
+#! /bin/bash
+# Script to delete all existing containers connected to SeoulAir microservices at once.
+
+
+root_dir="./"
+
+if [ "$#" -gt 1 ]; then
+	echo "Too many arguments. "
+	echo "Script has only one optional parameter and that is the root of the repository group"
+	exit 1
+fi
+
+if [ "$#" -eq 1 ] && ! [ -d "$1" ]; then
+	  echo "Usage: $1 DIRECTORY" >&2
+	  exit 1
+else
+	root_dir=$1
+fi
+
+if [ ! -d "./seoulair-device" ]; then
+	echo "SeoulAir.Device microservice is missing or it not on right place."
+	exit 1
+fi
+
+if [ ! -d "./seoulair-command" ]; then
+	echo "SeoulAir.Command microservice is missing or it not on right place."
+	exit 1
+fi
+
+if [ ! -d "./seoulair-analytics" ]; then
+	echo "SeoulAir.Analytics microservice is missing or it not on right place."
+	exit 1
+fi
+
+if [ ! -d "./seoulair-data" ]; then
+	echo "SeoulAir.Data microservice is missing or it not on right place."
+	exit 1
+fi
+
+#if [ ! -d "./seoulair-gateway" ]; then
+#	echo "SeoulAir.Gateway microservice is missing or it not on right place."
+#	exit 1
+#fi
+
+if systemctl status docker | grep -q 'inactive (dead)'; then
+	echo "Docker service is not running. Check if everything is ok with docker deamon."
+	exit 1
+fi
+
+if docker-compose --version | grep -q 'command not found'; then
+	echo "In order to use automation script you must have docker-compose installed."
+	exit 1
+fi
+
+docker-compose -f ./seoulair-device/docker-compose.yml down
+docker-compose -f ./seoulair-command/docker-compose.yml down
+docker-compose -f ./seoulair-analytics/docker-compose.yml down
+docker-compose -f ./seoulair-data/docker-compose.yml down
+#docker-compose -f ./seoulair-gateway/docker-compose.yml down
---
+++ shell-scripts/seoulair-start.sh
@@ -0,0 +1,70 @@
+#! /bin/bash
+# Script to run all SeoulAir microservices at once.
+
+root_dir="./"
+
+if [ "$#" -gt 1 ]; then
+	echo "Too many arguments. "
+	echo "Script has only one optional parameter and that is the root of the repository group"
+	exit 1
+fi
+
+if [ "$#" -eq 1 ] && ! [ -d "$1" ]; then
+	  echo "Usage: $1 DIRECTORY" >&2
+	  exit 1
+else
+	root_dir=$1
+fi
+
+if [ ! -d "$root_dir/seoulair-device" ]; then
+	echo "SeoulAir.Device microservice is missing or it not on right place."
+	exit 1
+fi
+
+if [ ! -d "$root_dir/seoulair-command" ]; then
+	echo "SeoulAir.Command microservice is missing or it not on right place."
+	exit 1
+fi
+
+if [ ! -d "$root_dir/seoulair-analytics" ]; then
+	echo "SeoulAir.Analytics microservice is missing or it not on right place."
+	exit 1
+fi
+
+if [ ! -d "$root_dir/seoulair-data" ]; then
+	echo "SeoulAir.Data microservice is missing or it not on right place."
+	exit 1
+fi
+
+#if [ ! -d "./seoulair-gateway" ]; then
+#	echo "SeoulAir.Gateway microservice is missing or it not on right place."
+#	exit 1
+#fi
+
+if systemctl status docker | grep -q 'inactive (dead)'; then
+	echo "Docker service is not running. Check if everything is ok with docker deamon."
+	exit 1
+fi
+
+if docker-compose --version | grep -q 'command not found'; then
+	echo "In order to use automation script you must have docker-compose installed."
+	exit 1
+fi
+
+#Starting SeoulAir Device
+docker-compose -f $root_dir/seoulair-device/docker-compose.yml kill
+docker-compose -f $root_dir/seoulair-device/docker-compose.yml up -d --build
+
+#Starting SeoulAir Command
+docker-compose -f $root_dir/seoulair-command/docker-compose.yml kill
+docker-compose -f $root_dir/seoulair-command/docker-compose.yml up -d --build
+
+#Starting SeoulAir Analytics
+docker-compose -f $root_dir/seoulair-analytics/docker-compose.yml kill
+docker-compose -f $root_dir/seoulair-analytics/docker-compose.yml up -d --build
+
+#Starting SeoulAir Data
+docker-compose -f $root_dir/seoulair-data/docker-compose.yml kill
+docker-compose -f $root_dir/seoulair-data/docker-compose.yml up -d --build
+
+#docker-compose -f ./seoulair-gateway/docker-compose.yml up -d --build --remove-orphans
